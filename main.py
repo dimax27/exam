396417006c3e61689d4ce9c4e2095bb8f674159e
Дима <dimax27@gmail.com>
2022-11-26 15:40:02 +0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("Hello Dima")
+import sys
+from PyQt6.QtWidgets import QApplication, QDialog
+from ui_imagedialog import Ui_ImageDialog
+
+app = QApplication(sys.argv)
+window = QDialog()
+ui = Ui_ImageDialog()
+ui.setupUi(window)
+
+window.show()
+sys.exit(app.exec())
